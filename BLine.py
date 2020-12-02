981dfa23f103eb52bafef4cd3f9b2c8905b109fc
Muhamad Irfan <73976445+nafri1305@users.noreply.github.com>
2020-12-02 15:34:35 +0800

    Update BLine.py

--- BLine.py
+++ BLine.py
@@ -71,8 +71,8 @@ class CustomConfig(Config):
     # Number of classes (including background)
     NUM_CLASSES = 1 + 1  # Background + BLine
 
-    # Number of training steps per epoch
-    STEPS_PER_EPOCH = 100
+    # Number of training steps per epoch, original is 100
+    STEPS_PER_EPOCH = 300
 
     # Skip detections with < 90% confidence
     DETECTION_MIN_CONFIDENCE = 0.9
